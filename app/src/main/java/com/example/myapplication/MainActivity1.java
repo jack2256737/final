3abfe68deb35a3bde2a702902ec00f737153ea6a
jack2256737 <jack2256737@gmail.com>
2023-12-31 01:08:40 +0800

    up

--- app/src/main/java/com/example/myapplication/MainActivity1.java
+++ app/src/main/java/com/example/myapplication/MainActivity1.java
@@ -30,6 +30,7 @@ public class MainActivity1 extends AppCompatActivity {
             public void onClick(View view) {
                 intent_toPage2.putExtra("URL_key",1);
                 startActivity(intent_toPage2);
+
             }
         });
         btn_2.setOnClickListener(new View.OnClickListener() {
